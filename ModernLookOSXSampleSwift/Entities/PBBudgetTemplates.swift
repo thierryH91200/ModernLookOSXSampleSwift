9fea9ad6fab9a611004b37e787f96f4d22987460
Your name <your.email@example.com>
2019-05-25 09:34:42 +0200

    init

---
+++ ModernLookOSXSampleSwift/Entities/PBBudgetTemplates.swift
@@ -0,0 +1,130 @@
+//
+//  PBBudgetTemplates.swift
+//  ModernLookOSXSampleSwift
+//
+//  Created by thierry hentic on 25/05/2019.
+//  Copyright © 2019 thierry hentic. All rights reserved.
+//
+
+import Cocoa
+
+class PBBudgetTemplates: NSObject {
+    
+    func createHomeBudget() -> PBBudget? {
+        
+        
+        
+        var ret = PBBudget()
+        ret.name = "Home Budget"
+        var c: PBCategory? = nil
+        
+        c = createCategory(withName: "Monthly Bills", andParent: nil)
+        if let c = c {
+            ret.categories.append(c)
+        }
+        createSubCategories(withName: [
+            "Rent/Morgage",
+            "Phone",
+            "Internet",
+            "Cable TV",
+            "Electricity",
+            "Water",
+            "Natural Gas/Propane/Oil"
+            ], forParent: c)
+        
+        c = createCategory(withName: "Insurances", andParent: nil)
+        ret.categories.append(c)
+        createSubCategories(withName: ["Car Insurance", "Life Insurance", "Health Insurance"], forParent: c)
+        
+        c = createCategory(withName: "Pleasure", andParent: nil)
+        ret.categories.append(c)
+        createSubCategories(withName: [
+            "Coffee",
+            "Shopaholic",
+            "Restaurants",
+            "iTunes Spendings",
+            "App Store Spendings"
+            ], forParent: c)
+        
+        c = createCategory(withName: "Everyday Expenses", andParent: nil)
+        ret.categories.append(c)
+        createSubCategories(withName: [
+            "Groceries",
+            "Fuel",
+            "Spending Money",
+            "Medical",
+            "Clothing",
+            "Household Goods"
+            ], forParent: c)
+        
+        c = createCategory(withName: "Saving Goals", andParent: nil)
+        ret.categories.append(c)
+        createSubCategories(withName: ["Christmas Presents", "Birthdays", "Car Replacement", "Vacation"], forParent: c)
+        
+        c = createCategory(withName: "Debt", andParent: nil)
+        ret.categories.append(c)
+        createSubCategories(withName: ["Car Payment", "Student Loan Payment", "Personal Loan Payment"], forParent: c)
+        
+        return ret
+        
+        
+    }
+    
+    func createSmallOfficeBudget() -> PBBudget? {
+        
+        
+        
+        
+        let ret = PBBudget()
+        ret.name = "Office Budget"
+        let c: PBCategory? = nil
+        
+        c = createCategory(withName: "Marketing", andParent: nil)
+        ret.categories.append(c)
+        createSubCategories(withName: [
+            "Printing",
+            "AdWords",
+            "Facebook",
+            "Twitter",
+            "Referral Fees",
+            "Comissions"
+            ], forParent: c)
+        
+        c = createCategory(withName: "Services", andParent: nil)
+        ret.categories.append(c)
+        createSubCategories(withName: ["Accounting", "Legal", "Contractors"], forParent: c)
+        
+        c = createCategory(withName: "Office", andParent: nil)
+        ret.categories.append(c)
+        createSubCategories(withName: ["Rent", "Telephone/Fax", "Internet", "Cleaning"], forParent: c)
+        
+        c = createCategory(withName: "Travel", andParent: nil)
+        ret.categories.append(c)
+        createSubCategories(withName: ["Automobile", "Hotels/Lodging", "Airfare", "Other"], forParent: c)
+        
+        c = createCategory(withName: "Taxes & Licenses", andParent: nil)
+        ret.categories.append(c)
+        createSubCategories(withName: ["Unemployment", "Payroll", "Quaterlies"], forParent: c)
+        
+        return ret
+        
+        
+    }
+    
+    func createCategory(withName name: String?, andParent parent: PBCategory?) -> PBCategory? {
+        let ret = PBCategory()
+        ret.name = name
+        if parent != nil {
+            ret.parent = parent
+            parent?.subCategories.append(ret)
+        }
+        return ret
+    }
+    
+    func createSubCategories(withName names: [Any]?, forParent parent: PBCategory?) {
+        for name in names as? [String] ?? [] {
+            createCategory(withName: name, andParent: parent)
+        }
+    }
+    
+}
--- ModernLookOSXSampleSwift/MainWindowController.swift
+++ ModernLookOSXSampleSwift/MainWindowController.swift
@@ -63,15 +63,12 @@ class MainWindowController: NSWindowController , MLCalendarViewDelegate{
     }
     
     @IBAction func showCalendar(_ sender: Any) {
-        //        win.showContent(settingsView)
-        
         createCalendarPopover()
         let btn = sender as? NSButton
         let cellRect: NSRect? = btn?.bounds
         if let btn = btn {
             calendarPopover?.show(relativeTo: cellRect ?? NSRect.zero, of: btn, preferredEdge: .minX)
         }
-        
     }
     
     @IBAction func showAlert(_ sender: Any) {
@@ -133,5 +130,73 @@ class MainWindowController: NSWindowController , MLCalendarViewDelegate{
 //        let c = PBCategoryContent(budget: b)
 //        return c
 //    }
+//}
+
+struct Stack<Element> {
+    fileprivate var array: [Element] = []
+    
+    mutating func push(_ element: Element) {
+        array.append(element)
+    }
+    
+    mutating func pop() -> Element? {
+        return array.popLast()
+    }
+    
+    mutating func get(_ num : Int) -> Element? {
+        return array[ num ]
+    }
+
+    func peek() -> Element? {
+        return array.last
+    }
+    
+    var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    var count: Int {
+        return array.count
+    }
+    
+}
+
+struct Account {
+    
+    var name = "name"
+    var budgeted = true
+    
+    init () {
+    }
+    
+    init(name: String = "name", budgeted: Bool = true)
+    {
+        self.name = name
+        self.budgeted  = budgeted
+    }
+    
+}
+
+struct PBBudget {
+    
+    var name = "name"
+    var budgeted = true
+    
+    init () {
+    }
+    
+    init(name: String = "name", budgeted: Bool = true)
+    {
+        self.name = name
+        self.budgeted  = budgeted
+    }
+}
+
+//struct PBCategory {
+//    var budget: PBBudget?
+//    var parent: PBCategory? = nil
+//    var subCategories: [AnyHashable] = []
+//    var name = ""
+//    var seq: NSNumber?
 //}
 
